dadc98dc683b90f48368270239f52d425990f932
Nicholas J Humfrey <njh@aelius.com>
2012-08-19 11:17:14 +0100

    Added post-recieve-hook.sh script

---
+++ post-receive-hook.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# This script is automatically run on the remote server
+# after a change is pushed using git
+#
+
+echo "Running post-receive hook"
+
+# Discard any local changes
+unset GIT_DIR
+git reset --hard HEAD
+
+# Update dependencies
+composer install
