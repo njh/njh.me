667f9d44f7373e4982f8cb16a6b2cd714c262351
Nicholas J Humfrey <njh@aelius.com>
2012-08-20 23:10:05 +0100

    Added support for serving of FOAF document by serialising with EasyRdf

--- public/index.php
+++ public/index.php
@@ -1,8 +1,10 @@
 <?php
 
-set_include_path(get_include_path() . PATH_SEPARATOR . '../lib/');
-
 require '../vendor/autoload.php';
+
+# FIXME: work out how to autoload EasyRdf
+set_include_path(get_include_path() . PATH_SEPARATOR . '../vendor/njh/easyrdf/lib/');
+require 'EasyRdf.php';
 
 // Prepare app
 $app = new Slim(array(
@@ -11,13 +13,30 @@ $app = new Slim(array(
 
 // Setup routes
 $app->get('/', function () use ($app) {
-    $format = $app->respondTo('html', 'rdf');
+    $format = $app->respondTo('rdf', 'ttl', 'json');
     switch($format) {
         case 'html':
             return $app->redirect('http://www.aelius.com/njh/', 303);
         default:
-            return $app->redirect("http://www.aelius.com/njh/foaf.$format", 303);
-    }        
+            # FIXME: return absolute URL
+            return $app->redirect("foaf.$format", 303);
+    }
+});
+
+$app->get('/foaf:format', function () use ($app) {
+    $format = $app->respondTo('rdf', 'ttl', 'json');
+    $foaf = new EasyRdf_Graph();
+    $foaf->parseFile('../data/foaf.ttl', 'turtle');
+
+    # FIXME: serialise by file extension automatically
+    switch($format) {
+        case 'rdf':
+            return $app->response()->body( $foaf->serialise('rdfxml') );
+        case 'ttl':
+            return $app->response()->body( $foaf->serialise('turtle') );
+        case 'json':
+            return $app->response()->body( $foaf->serialise('json') );
+    }
 });
 
 // Run app
