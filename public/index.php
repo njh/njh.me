83bff67fbf7e1908ab24663bab2cbed1d64f9c81
Nicholas J Humfrey <njh@aelius.com>
2012-08-21 18:52:19 +0100

    Updated to latest version of EasyRdf - can now serialise based on the file extension.

--- public/index.php
+++ public/index.php
@@ -25,18 +25,10 @@ $app->get('/', function () use ($app) {
 
 $app->get('/foaf:format', function () use ($app) {
     $format = $app->respondTo('rdf', 'ttl', 'json');
+
     $foaf = new EasyRdf_Graph();
     $foaf->parseFile('../data/foaf.ttl', 'turtle');
-
-    # FIXME: serialise by file extension automatically
-    switch($format) {
-        case 'rdf':
-            return $app->response()->body( $foaf->serialise('rdfxml') );
-        case 'ttl':
-            return $app->response()->body( $foaf->serialise('turtle') );
-        case 'json':
-            return $app->response()->body( $foaf->serialise('json') );
-    }
+    $app->response()->body( $foaf->serialise($format) );
 });
 
 // Run app
